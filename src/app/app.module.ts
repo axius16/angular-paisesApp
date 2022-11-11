dbafe0784b1c56d9409d2622c7c02212331eb370
nalbarracin <naza16@gmail.com>
2022-11-11 17:31:02 -0300

    Refactorizacion del sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
